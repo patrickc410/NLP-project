89e375fba9a8286be96935b9845bf33c6e20bcb6
Patrick Crawford <pac410@gmail.com>
2022-11-10 20:16:35 -0500

    update data loading functions

--- src/nlp_proj/data_loader.py
+++ src/nlp_proj/data_loader.py
@@ -1,7 +1,73 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
-from typing import List
+from typing import List, Tuple
+import logging
+
+
+def load_wiki_manual_tsv(data_filepath: str) -> Tuple[pd.DataFrame, List[str]]:
+    """Load Wiki-Manual TSV file as DataFrame
+
+    Args:
+        data_filepath (str): path to .tsv
+
+    Returns:
+        Tuple[pd.DataFrame, List[str]]: dataframe, list of column names
+    """
+    columns = [
+        "label",
+        "simple-sent-index",
+        "complex-sent-index",
+        "simple-sent",
+        "complex-sent",
+        "GLEU-score",
+    ]
+    try:
+        df = pd.read_csv(data_filepath, delimiter="\t", names=columns, header=None)
+    except Exception:
+        data = []
+        error_count = 0
+        with open(data_filepath, "r") as f:
+            for line in f.readlines():
+                split = line.split("\t")
+                if len(split) != len(columns):
+                    error_count += 1
+                else:
+                    entry = {col: val for col, val in zip(columns, split)}
+                    data.append(entry)
+        if error_count > 0:
+            logging.warn(f"Unable to load {error_count} lines of file '{data_filepath}'")
+        df = pd.DataFrame(data)
+
+    return df, columns
+
+
+def concat_wiki_manual_sentences(
+    df: pd.DataFrame, drop_duplicates: bool = True
+) -> pd.DataFrame:
+    """Make a dataframe with rows of (sent-index, sent)
+    where all the ("simple-sent-index", "simple-sent") and ("complex-sent-index", "complex-sent")
+    pairs have been concatenated along the same axis
+
+    Args:
+        df (pd.DataFrame): dataframe from load_wiki_manual_tsv()
+        drop_duplicates (bool): whether to drop duplicates
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    df_simple = df[["simple-sent-index", "simple-sent"]]
+    df_simple = df_simple.rename(
+        columns={"simple-sent-index": "sent-index", "simple-sent": "sent"}
+    )
+    df_complex = df[["complex-sent-index", "complex-sent"]]
+    df_complex = df_complex.rename(
+        columns={"complex-sent-index": "sent-index", "complex-sent": "sent"}
+    )
+    df_out = pd.concat((df_simple, df_complex), axis=0, ignore_index=True)
+    if drop_duplicates is True:
+        df_out = df_out.drop_duplicates(ignore_index=True)
+    return df_out
 
 
 class WikiManualDataset(Dataset):
@@ -9,18 +75,7 @@ class WikiManualDataset(Dataset):
 
     def __init__(self, data_filepath: str) -> None:
         self.data_filepath = data_filepath
-
-        self.columns = [
-            "label",
-            "simple-sent-index",
-            "complex-sent-index",
-            "simple-sent",
-            "complex-sent",
-            "GLEU-score",
-        ]
-        self.df = pd.read_csv(
-            self.data_filepath, delimiter="\t", names=self.columns, header=None
-        )
+        self.df, self.columns = load_wiki_manual_tsv(self.data_filepath)
 
         super().__init__()
 
@@ -47,4 +102,3 @@ class WikiManualSentenceDataset(WikiManualDataset):
             return row[["simple-sent-index", "simple-sent"]].tolist()
         else:
             return row[["complex-sent-index", "complex-sent"]].tolist()
-
