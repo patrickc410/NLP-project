16e300eb4d8ca24b97d8fda0b5ffa9b408629a88
Patrick Crawford <pac410@gmail.com>
2022-11-10 20:23:05 -0500

    remove example test function, replace with another example test function

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -2,12 +2,12 @@
 
 from pathlib import Path
 
-from nlp_proj.data_loader import (
-    function
-)
+from nlp_proj.data_loader import WikiManualDataset, WikiManualSentenceDataset
 
 DATA_ROOT = Path(__file__).resolve().parent.parent / "data"
 
 
-def test_function():
-    assert function() is None
+def test_WikiManualDataset():
+
+    dataset = WikiManualDataset("../wiki-auto/wiki-manual/dev.tsv")
+    assert dataset is not None
